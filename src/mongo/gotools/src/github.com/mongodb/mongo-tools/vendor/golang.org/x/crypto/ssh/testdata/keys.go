a22c3b81d21dbea3b07f51e5982e6d7e4295b422
Harsh Pruthi <68766440+pr0thi@users.noreply.github.com>
2025-10-19 00:23:09 +0530

    StackGuard Auto-Remediation: Secure secrets in src/mongo/gotools/src/github.com/mongodb/mongo-tools/vendor/golang.org/x/crypto/ssh/testdata/keys.go

--- src/mongo/gotools/src/github.com/mongodb/mongo-tools/vendor/golang.org/x/crypto/ssh/testdata/keys.go
+++ src/mongo/gotools/src/github.com/mongodb/mongo-tools/vendor/golang.org/x/crypto/ssh/testdata/keys.go
@@ -4,8 +4,53 @@
 
 package testdata
 
-var PEMBytes = map[string][]byte{
-	"dsa": []byte(`-----BEGIN DSA PRIVATE KEY-----
+import (
+	"log"
+	"os"
+
+	"github.com/hashicorp/vault/api"
+)
+
+var PEMBytes map[string][]byte
+
+func init() {
+	vaultAddr := os.Getenv("VAULT_ADDR")
+	vaultToken := os.Getenv("VAULT_TOKEN")
+
+	config := api.DefaultConfig()
+	if vaultAddr != "" {
+		config.Address = vaultAddr
+	}
+
+	client, err := api.NewClient(config)
+	if err != nil {
+		log.Fatalf("failed to create Vault client: %v", err)
+	}
+
+	if vaultToken != "" {
+		client.SetToken(vaultToken)
+	}
+
+	secret, err := client.Logical().Read("secret/data/pr0thi-mongo-ssh-testdata-keys-256cb2e9")
+	if err != nil {
+		log.Fatalf("failed to read secret from Vault: %v", err)
+	}
+	if secret == nil || secret.Data == nil || secret.Data["data"] == nil {
+		log.Fatalf("secret not found or empty at 'secret/data/pr0thi-mongo-ssh-testdata-keys-256cb2e9'")
+	}
+
+	data, ok := secret.Data["data"].(map[string]interface{})
+	if !ok {
+		log.Fatalf("invalid secret data format")
+	}
+
+	rsaKey, ok := data["secret"].(string)
+	if !ok {
+		log.Fatalf("secret key 'secret' not found or not a string in Vault")
+	}
+
+	PEMBytes = map[string][]byte{
+		"dsa": []byte(`-----BEGIN DSA PRIVATE KEY-----
 MIIBuwIBAAKBgQD6PDSEyXiI9jfNs97WuM46MSDCYlOqWw80ajN16AohtBncs1YB
 lHk//dQOvCYOsYaE+gNix2jtoRjwXhDsc25/IqQbU1ahb7mB8/rsaILRGIbA5WH3
 EgFtJmXFovDz3if6F6TzvhFpHgJRmLYVR8cqsezL3hEZOvvs2iH7MorkxwIVAJHD
@@ -18,26 +63,18 @@ nOwuxb0Kce+gWI8voWcqC9cyRm09jGzu2Ab3Bhtpg8JJ8L7gS3MRZK4CFEx4UAfY
 Fmsr0W6fHB9nhS4/UXM8
 -----END DSA PRIVATE KEY-----
 `),
-	"ecdsa": []byte(`-----BEGIN EC PRIVATE KEY-----
+		"ecdsa": []byte(`-----BEGIN EC PRIVATE KEY-----
 MHcCAQEEINGWx0zo6fhJ/0EAfrPzVFyFC9s18lBt3cRoEDhS3ARooAoGCCqGSM49
 AwEHoUQDQgAEi9Hdw6KvZcWxfg2IDhA7UkpDtzzt6ZqJXSsFdLd+Kx4S3Sx4cVO+
 6/ZOXRnPmNAlLUqjShUsUBBngG0u2fqEqA==
 -----END EC PRIVATE KEY-----
 `),
-	"rsa": []byte(`-----BEGIN RSA PRIVATE KEY-----
-MIIBOwIBAAJBALdGZxkXDAjsYk10ihwU6Id2KeILz1TAJuoq4tOgDWxEEGeTrcld
-r/ZwVaFzjWzxaf6zQIJbfaSEAhqD5yo72+sCAwEAAQJBAK8PEVU23Wj8mV0QjwcJ
-tZ4GcTUYQL7cF4+ezTCE9a1NrGnCP2RuQkHEKxuTVrxXt+6OF15/1/fuXnxKjmJC
-nxkCIQDaXvPPBi0c7vAxGwNY9726x01/dNbHCE0CBtcotobxpwIhANbbQbh3JHVW
-2haQh4fAG5mhesZKAGcxTyv4mQ7uMSQdAiAj+4dzMpJWdSzQ+qGHlHMIBvVHLkqB
-y2VdEyF7DPCZewIhAI7GOI/6LDIFOvtPo6Bj2nNmyQ1HU6k/LRtNIXi4c9NJAiAr
-rrxx26itVhJmcvoUhOjwuzSlP2bE5VHAvkGB352YBg==
------END RSA PRIVATE KEY-----
-`),
-	"user": []byte(`-----BEGIN EC PRIVATE KEY-----
+		"rsa": []byte(rsaKey),
+		"user": []byte(`-----BEGIN EC PRIVATE KEY-----
 MHcCAQEEILYCAeq8f7V4vSSypRw7pxy8yz3V5W4qg8kSC3zJhqpQoAoGCCqGSM49
 AwEHoUQDQgAEYcO2xNKiRUYOLEHM7VYAp57HNyKbOdYtHD83Z4hzNPVC4tM5mdGD
 PLL8IEwvYu2wq+lpXfGQnNMbzYf9gspG0w==
 -----END EC PRIVATE KEY-----
 `),
+	}
 }
